76ed6fa970b1277a7953db13462cfcd37d3c6d47
Juliane Reis <julianereisb@gmail.com>
2021-06-18 06:29:04 -0300

    Exercise 6 done

---
+++ 06. Exercise/06.Answer.js
@@ -0,0 +1,4 @@
+// https://www.delltechnologies.com/pt-br/what-we-do/multi-cloud-flexibility.htm
+
+var elem = document.querySelector('#solncat-hero');
+document.querySelector('#solncat-offering').prepend(elem);
