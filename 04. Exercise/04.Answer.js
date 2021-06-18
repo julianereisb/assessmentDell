2d7d9726b96bda296dc84b7636472854f166b55c
Juliane Reis <julianereisb@gmail.com>
2021-06-18 05:13:27 -0300

    Exercise 4 done

---
+++ 04. Exercise/04.Answer.js
@@ -0,0 +1,20 @@
+const breakpoints = {xxlarge:1600,xlarge:1280,large:980,medium:768,small:640};
+$(window).resize(function() {
+    let winSize = ($(window).width());
+    $.each(breakpoints, function(index, obj) {
+        if(obj >= winSize && winSize <= 768) {
+                $('#tt-content .header-img').css({
+                "background":"url(https://placekitten.com/1280/425) no-repeat center center/cover",
+                "min-height": "250px",
+                "height": "inherit"
+            });
+
+        } else {
+            $('#tt-content .hero .rendition.content').css({
+                "background":"url(https://placekitten.com/1280/425) no-repeat center center/cover",
+                "min-height": "425px",
+                "height": "inherit"
+            });
+        }
+    });
+});
